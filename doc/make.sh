d9b32825f550bdb613fb90e3ca1f0d6b36015918
Andreas Linz <klingt.net@gmail.com>
2014-04-23 13:08:32 +0200

    moved src folder to content

--- doc/make.sh
+++ doc/make.sh
@@ -3,17 +3,23 @@
 ROOT=$(pwd)
 BUILD_DIR=$ROOT/build
 OUTPUT_DIR=$ROOT/output
-SRC=$ROOT/src
+SRC=$ROOT/content
 # extension is superfluous
-TEXFILE=template
+TEXFILE=main
 XELATEX_OPTS=-halt-on-error
 
 # sync src to build
 rsync --archive --delete-before --update --quiet $SRC/ $BUILD_DIR/
 
+if [ ! -z $1 ]; then
+    TEXFILE = $1
+    echo $TEXFILE
+fi
+
 # build pdf from tex
 pushd $BUILD_DIR
 xelatex $XELATEX_OPTS -no-pdf $TEXFILE
+if $? == 0; then; return 1; fi
 #biber $TEXFILE
 #makeglossaries $TEXFILE
 xelatex $XELATEX_OPTS $TEXFILE > /dev/null
