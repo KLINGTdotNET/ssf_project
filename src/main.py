fcfac97af787177fcead81e47eef5f45b6d5e2de
Andreas Linz <klingt.net@gmail.com>
2014-05-01 17:30:52 +0200

    refactored url read method to spereate file

--- src/main.py
+++ src/main.py
@@ -4,8 +4,21 @@ from tools import cli, read_url
 
 def main():
     args = cli.get_args()
-    print(read_url.read(args.url))
-    pass
+    content = read_url.read(args.url)
+    print(content)
 
 if __name__ == '__main__':
     main()
+
+"""
+from lxml import etree
+
+filepath = "/home/andreas/personal/dev/python/ssf_project/resources/facebook.xsd"
+url = 'http://api.facebook.com/1.0/facebook.xsd'
+
+with open(filepath, 'r') as file:
+    xmlschema_doc = etree.parse(file)
+    xmlschema = etree.XMLSchema(xmlschema_doc)
+
+print(etree.tostring(xmlschema_doc, pretty_print=True))
+"""
