cdaabd069c69d85db72923a0e35fd3b6969d2b50
Andreas Linz <klingt.net@gmail.com>
2014-05-02 19:24:57 +0200

    changed package name from src to xsdtocls

---
+++ src/tests/test_tools.py
@@ -0,0 +1,5 @@
+from tools import cli
+
+class TestCLI:
+    def test_minimal(self):
+        pass
--- xsdtocls/main.py
+++ xsdtocls/main.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 from tools import cli, read_url
+import generator
 
 def main():
     args = cli.get_args()
     content = read_url.read(args.url)
-    print(content)
 
 if __name__ == '__main__':
     main()
