436e655db487bff64a0164896fe9abe3e6383fb8
Andreas Linz <klingt.net@gmail.com>
2014-05-13 12:40:30 +0200

    added XSD namespace check

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python3 xsdtocls/main.py $1 resources/facebook.xsd ~/personal/tmp/dump
+python3 xsdtocls/main.py $1 resources/spreadshirt.xsd ~/personal/tmp/dump
--- xsdtocls/processor/mapper.py
+++ xsdtocls/processor/mapper.py
@@ -8,26 +8,37 @@ from collections import deque
 import logging
 
 def map(tree):
-    root = tree.getroot()
-    xsdns = root.nsmap['xsd']
-    ns = root.attrib['targetNamespace']
-    qualified = True if root.attrib['elementFormDefault'] == 'qualified' else False
-    print('qualified: {}\ntarget-namespace: {}\nnamespaces: {}'.format(qualified, ns, root.nsmap))
+    schema = tree.getroot()
+    xsdns = None
+    for prefix, ns in schema.nsmap.items():
+        if ns == 'http://www.w3.org/2001/XMLSchema':
+            xsdns = {'prefix': prefix, 'ns': ns}
+    if not xsdns:
+        logging.error('XSD namespace is not defined!')
+        raise SystemExit('Terminating ...')
+    tns = schema.attrib['targetNamespace']
+    qualified = True if schema.attrib['elementFormDefault'] == 'qualified' else False
+    print('qualified: {}\ntarget-namespace: {}\nnamespaces: {}'.format(qualified, tns, schema.nsmap))
     doc = {}
-    doc['attributes'] = [_ for _ in root.iterchildren(tag='{%s}attribute' % xsdns)]
-    doc['elements'] = [_ for _ in root.iterchildren(tag='{%s}element' % xsdns)]
-    doc['complex_types'] = [_ for _ in root.iterchildren(tag='{%s}complexType' % xsdns)]
-    doc['simple_types'] = [_ for _ in root.iterchildren(tag='{%s}simpleType' % xsdns)]
+    doc['attributes'] = [_ for _ in schema.iterchildren(tag='{%s}attribute' % xsdns)]
+    doc['elements'] = [_ for _ in schema.iterchildren(tag='{%s}element' % xsdns)]
+    doc['complex_types'] = [_ for _ in schema.iterchildren(tag='{%s}complexType' % xsdns)]
+    doc['simple_types'] = [_ for _ in schema.iterchildren(tag='{%s}simpleType' % xsdns)]
     #
     for k, v in doc.items():
         logging.debug('{1} {0}'.format(k, len(v)))
     #
-    simple_types(doc['simple_types'])
+    simple_types(doc['simple_types'], tns)
 
-def simple_types(simples):
+def simple_types(simples, tns):
     for simple in simples:
         if 'name' in simple.attrib.keys():
-            #print(simple.attrib['name'])
-            pass
+            name = simple.attrib['name']
+            # check for elements
+            if len(simple):
+                for child in simple:
+                    print(child.tag, child.attrib)
+            else:
+                logging.debug('SimpleType "{}" is empty'.format(name))
         else:
-            logging.debug('{}')
+            logging.debug('Nameless SimpleType in "{}"'.format(tns))
