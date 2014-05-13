de35bef6f0f49dc3c95346a9c089c581cf372e15
Andreas Linz <klingt.net@gmail.com>
2014-05-13 14:47:23 +0200

    added namespace qualifier

--- xsdtocls/processor/mapper.py
+++ xsdtocls/processor/mapper.py
@@ -20,23 +20,26 @@ def map(tree):
     qualified = True if schema.attrib['elementFormDefault'] == 'qualified' else False
     print('qualified: {}\ntarget-namespace: {}\nnamespaces: {}'.format(qualified, tns, schema.nsmap))
     doc = {}
-    doc['attributes'] = [_ for _ in schema.iterchildren(tag='{%s}attribute' % xsdns['ns'])]
-    doc['elements'] = [_ for _ in schema.iterchildren(tag='{%s}element' % xsdns['ns'])]
-    doc['complex_types'] = [_ for _ in schema.iterchildren(tag='{%s}complexType' % xsdns['ns'])]
-    doc['simple_types'] = [_ for _ in schema.iterchildren(tag='{%s}simpleType' % xsdns['ns'])]
+    doc['attributes'] = [_ for _ in schema.iterchildren(tag=qualify('attribute', xsdns['ns']))]
+    doc['elements'] = [_ for _ in schema.iterchildren(tag=qualify('element', xsdns['ns']))]
+    doc['complex_types'] = [_ for _ in schema.iterchildren(tag=qualify('complexType', xsdns['ns']))]
+    doc['simple_types'] = [_ for _ in schema.iterchildren(tag=qualify('simpleType', xsdns['ns']))]
     for k, v in doc.items():
         logging.debug('{1} {0}'.format(k, len(v)))
-    simple_types(doc['simple_types'], tns)
+    simple_types(doc['simple_types'], tns, xsdns)
 
-def simple_types(simples, tns):
+def simple_types(simples, tns, xsdns):
     for simple in simples:
         if 'name' in simple.attrib.keys():
             name = simple.attrib['name']
-            # check for elements
             if len(simple):
                 for child in simple:
-                    print(child.tag, child.attrib)
+                    if child.tag == qualify('restriction', xsdns['ns']):
+                        print(child.attrib)
             else:
                 logging.debug('SimpleType "{}" is empty'.format(name))
         else:
             logging.debug('Nameless SimpleType in "{}"'.format(tns))
+
+def qualify(name, ns):
+    return '{{{}}}{}'.format(ns, name)
