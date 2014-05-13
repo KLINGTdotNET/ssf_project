35cc3e43c54c2d46c1c398dec45c020020becdfd
Andreas Linz <klingt.net@gmail.com>
2014-05-13 12:25:15 +0200

    formatted output

--- xsdtocls/processor/mapper.py
+++ xsdtocls/processor/mapper.py
@@ -10,7 +10,9 @@ import logging
 def map(tree):
     root = tree.getroot()
     xsdns = root.nsmap['xsd']
-    print(root.tag, root.attrib)
+    ns = root.attrib['targetNamespace']
+    qualified = True if root.attrib['elementFormDefault'] == 'qualified' else False
+    print('qualified: {}\ntarget-namespace: {}\nnamespaces: {}'.format(qualified, ns, root.nsmap))
     doc = {}
     doc['attributes'] = [_ for _ in root.iterchildren(tag='{%s}attribute' % xsdns)]
     doc['elements'] = [_ for _ in root.iterchildren(tag='{%s}element' % xsdns)]
@@ -18,7 +20,7 @@ def map(tree):
     doc['simple_types'] = [_ for _ in root.iterchildren(tag='{%s}simpleType' % xsdns)]
     #
     for k, v in doc.items():
-        logging.debug('{} has {} elements'.format(k, len(v)))
+        logging.debug('{1} {0}'.format(k, len(v)))
     #
     simple_types(doc['simple_types'])
 
@@ -26,7 +28,6 @@ def simple_types(simples):
     for simple in simples:
         if 'name' in simple.attrib.keys():
             #print(simple.attrib['name'])
-            logging.debug(simple)
             pass
         else:
             logging.debug('{}')
