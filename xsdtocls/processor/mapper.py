100c514d1cf6df22423201a00f68a05ca61fa573
Andreas Linz <klingt.net@gmail.com>
2014-05-15 16:35:10 +0200

    removed executer permissions from files and implemented parts of the model mappers

--- xsdtocls/processor/mapper.py
+++ xsdtocls/processor/mapper.py
@@ -20,74 +20,105 @@ def map(tree):
     print('qualified: {}\ntarget-namespace: {}\nnamespaces: {}'.format(qualified, tns, root.nsmap))
     schema = {}
     # note that if the schema element is the parent node, then the actual note contains a declaration
+    schema['imports'] = [_ for _ in root.iterchildren(tag=qualify('import', xsdns['ns']))]
     schema['attributes'] = [_ for _ in root.iterchildren(tag=qualify('attribute', xsdns['ns']))]
     schema['elements'] = [_ for _ in root.iterchildren(tag=qualify('element', xsdns['ns']))]
     schema['complex_types'] = [_ for _ in root.iterchildren(tag=qualify('complexType', xsdns['ns']))]
     schema['simple_types'] = [_ for _ in root.iterchildren(tag=qualify('simpleType', xsdns['ns']))]
     for k, v in schema.items():
         logging.debug('{1} {0}'.format(k, len(v)))
-    attributes(schema['attributes'], tns, xsdns)
-    simple_types(schema['simple_types'], tns, xsdns)
-    complex_types(schema['complex_types'], tns, xsdns)
-    elements(schema['elements'], tns, xsdns)
 
-def attributes(attributes, tns, xsdns):
+    for schema_import in schema['imports']:
+        map_import(schema_import, tns, xsdns)
+    for attribute in schema['attributes']:
+        map_attribute(attribute, tns, xsdns)
+    for simple_type in schema['simple_types']:
+        map_simple_type(simple_type, tns, xsdns)
+    for complex_type in schema['complex_types']:
+        map_complex_type(complex_type, tns, xsdns)
+    for element in schema['elements']:
+        map_element(element, tns, xsdns)
+
+def map_import(schema_import, tns, xsdns):
+    print(schema_import.tag, schema_import.attrib, len(schema_import))
+
+def map_type(type, tns, xsdns):
+    prefix, name = type.split(':')
+    ns = ''
+    if prefix == 'tns':
+        ns = tns
+    elif prefix == xsdns['prefix']:
+        ns = xsdns['ns']
+    else:
+        logging.debug('Unknown namespace prefix: {}'.format(prefix))
+    return {
+        'prefix': prefix,
+        'name': name,
+        'ns': ns
+    }
+
+def map_attribute(attribute, tns, xsdns):
     '''
     see http://www.w3.org/TR/xmlschema-1/#cAttribute_Declarations
     '''
-    for attribute in attributes:
-        if 'name' in attribute.attrib:
-            name = attribute.attrib['name']
-            if 'fixed' in attribute.attrib:
-                fixed = attribute.attrib['fixed']
-            if 'default' in attribute.attrib:
-                default = attribute.attrib['default']
-            if 'use' in attribute.attrib:
-                # optional, required or prohibited
-                use = attribute.attrib['use']
-            if len(attribute):
-                # type definition in child nodes (must be a SimpleType)
-                pass
+    content = {}
+    if 'name' in attribute.attrib:
+        content['name'] = attribute.attrib['name']
+        if 'fixed' in attribute.attrib:
+            content['fixed'] = attribute.attrib['fixed']
+        if 'default' in attribute.attrib:
+            content['default'] = attribute.attrib['default']
+        if 'use' in attribute.attrib:
+            # optional, required or prohibited
+            content['use'] = attribute.attrib['use']
+        if 'type' in attribute.attrib:
+            content['type'] = map_type(attribute.attrib['type'], tns, xsdns)
+        elif len(attribute):
+            if attribute[0].tag == qualify('simpleType', xsdns['ns']):
+                content['type'] = map_simple_type(attribute[0], tns, xsdns)
             else:
-                type = attribute.attrib['type']
-        elif 'ref' in attribute.attrib:
-            console.debug('Implement me!')
+                logging.debug('Bad attribute type {}'.format(attribute[0].tag))
         else:
-            console.debug('Wether "reference" nor a "explicit declaration"!')
+            logging.debug('Wether type-declration nor type-definition in attribute {}'.format(name))
+    elif 'ref' in attribute.attrib:
+        print(attribute.attrib.items())
+        print(len(attribute.attrib))
+        content['ref'] = attribute.attrib['ref']
+        print(attribute.attrib)
+        logging.debug('Implement me!')
+    else:
+        logging.debug('Wether "reference" nor a "explicit declaration"!')
+    return content
 
-
-def simple_types(simples, tns, xsdns):
+def map_simple_type(simple, tns, xsdns):
     '''
     see http://www.w3.org/TR/2004/REC-xmlschema-2-20041028/datatypes.html#element-simpleType
     '''
-    simple_types = []
-    for simple in simples:
-        if 'name' in simple.attrib.keys():
-            name = simple.attrib['name']
-            content = {}
-            if len(simple):
-                for child in simple:
-                    if child.tag == qualify('restriction', xsdns['ns']):
-                        content = simple_type_restriction(child, xsdns)
-                    elif child.tag == qualify('list', xsdns['ns']):
-                        content = simple_type_list()
-                    elif child.tag ==qualify('union', xsdns['ns']):
-                        content = simple_type_union()
-                    else:
-                        logging.debug('Unexpected element "{}" in "{}"'.format(child.tag, name))
+    content = {}
+    if 'name' in simple.attrib.keys():
+        content['name'] = simple.attrib['name']
+    elif simple.getparent().tag is qualify('schema', xsdns['ns']):
+        logging.warn('Anonymous simpleType as direct child of "schema" element!')
+        return {}
+    if len(simple):
+        for child in simple:
+            if child.tag == qualify('restriction', xsdns['ns']):
+                content = simple_type_restriction(child, tns, xsdns)
+            elif child.tag == qualify('list', xsdns['ns']):
+                content = simple_type_list()
+            elif child.tag == qualify('union', xsdns['ns']):
+                content = simple_type_union()
             else:
-                logging.debug('SimpleType "{}" is empty'.format(name))
-        else:
-            logging.debug('Nameless SimpleType in "{}"'.format(tns))
-        # create object of class SimpleType
-        simple_types.append((name, content))
-    return simple_types
+                logging.debug('Unexpected element "{}" in "{}"'.format(child.tag, name))
+    else:
+        logging.debug('SimpleType "{}" is empty'.format(name))
+    return content
 
-def simple_type_restriction(restriction, xsdns):
+def simple_type_restriction(restriction, tns, xsdns):
     '''
     see http://www.w3.org/TR/2004/REC-xmlschema-2-20041028/datatypes.html#element-restriction
     '''
-    content = {'base': restriction.attrib['base']}
+    content = {'base': map_type(restriction.attrib['base'], tns, xsdns)}
     if len(restriction):
         for child in restriction:
             tag = unqualify(child.tag, xsdns['ns'])
@@ -115,24 +146,78 @@ def simple_type_list():
 def simple_type_union():
     logging.warn('Implement me!')
 
-def complex_types(complexs, tns, xsdns):
+def map_complex_type(complex, tns, xsdns):
     '''
     see http://www.w3.org/TR/xmlschema-1/#Complex_Type_Definitions
     '''
-    for complex in complexs:
-        if 'name' in complex.attrib:
-            name = complex.attrib['name']
-            if 'abstract' in complex.attrib:
-                abstract = True if complex.attrib['abstract'].lower() == "true" else False
-            if 'mixed' in complex.attrib:
-                mixed = True if complex.attrib['mixed'].lower() == "true" else False
-            if 'final' in complex.attrib:
-                final = complex.attrib['final'].split()
-        else:
+    content = {}
+    if 'name' in complex.attrib:
+        content['name'] = complex.attrib['name']
+        if 'abstract' in complex.attrib:
+            content['abstract'] = True if complex.attrib['abstract'].lower() == "true" else False
+        if 'mixed' in complex.attrib:
+            content['mixed'] = True if complex.attrib['mixed'].lower() == "true" else False
+        if 'final' in complex.attrib:
+            content['final'] = complex.attrib['final'].split()
+        if 'base' in complex.attrib:
+            content['base'] = complex.attrib['base']
+        if len(complex):
+            for child in complex:
+                # attribute, complexContent, sequence, simpleContent, all
+                unqualified = unqualify(child.tag, xsdns['ns'])
+                if 'attribute' == unqualified:
+                    if not 'attributes' in content:
+                        content['attributes'] = []
+                    content['attributes'].append(map_attribute(child, tns, xsdns))
+                elif 'complexContent' == unqualified:
+                    pass
+                elif 'sequence' == unqualified:
+                    # elements must appear in that order
+                    pass
+                elif 'simpleContent' == unqualified:
+                    if 'content' not in content:
+                        content['content'] = []
+                    content['content'].append(map_simple_content(child, tns, xsdns))
+                elif 'all' == unqualified:
+                    # elements can occur in any order
+                    pass
+                else:
+                    logging.debug('Unexpected element "{}"'.format(unqualified))
             pass
-            # anonymous (local) type definition
+    else:
+        pass
+        # anonymous (local) type definition
 
-def elements(elements, tns, xsdns):
+def map_simple_content(simple, tns, xsdns):
+    '''
+    see http://msdn.microsoft.com/de-de/library/ms256106(v=vs.110).aspx
+    '''
+    content = {}
+    unqualified = unqualify(simple[0].tag, xsdns['ns'])
+    if unqualified == 'extension':
+        content['extends'] = {
+            'base': map_type(simple[0].attrib['base'], tns, xsdns)
+        }
+    elif unqualified == 'restriction':
+        content['restricts'] = {
+            'base': map_type(simple[0].attrib['base'], tns, xsdns)
+        }
+    else:
+        logging.debug('Bad simpleContent definition with direct child {}'.format(simple[0].tag))
+    if len(simple[0]):
+        for child in simple[0]:
+            unqualified = unqualify(child.tag, xsdns['ns'])
+            if unqualified == 'attribute':
+                if 'attributes' not in content:
+                    content['attributes'] = []
+                content['attributes'].append(map_attribute(child, tns, xsdns))
+            else:
+                print(child.tag, child.attrib)
+    else:
+        logging.debug('Empty simpleContent!')
+    return content
+
+def map_element(element, tns, xsdns):
     pass
 
 def qualify(name, ns):
